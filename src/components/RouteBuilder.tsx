e789fa8c6e7096c711397cf5bb744f9c1269e57c
Paul Ilaga <pgilaga@gmail.com>
2024-03-22 10:34:44 +0800

    Add route state

--- src/components/RouteBuilder.tsx
+++ src/components/RouteBuilder.tsx
@@ -9,18 +9,25 @@ const Map = dynamic(() => import("@/components/Map"), {
   ssr: false,
 });
 
+interface Waypoints {
+  name: string;
+  latlng: LatLngExpression;
+}
+
 interface RouteBuilderProps {}
 
 const RouteBuilder: React.FC<RouteBuilderProps> = () => {
   const initPos: LatLngExpression = [14.599512, 120.984222];
-  const [coords, setCoords] = useState<LatLngExpression | undefined>(undefined);
-  const handleMapClick = (coords: LatLngExpression) => setCoords(coords);
+
+  const [route, setRoute] = useState<Waypoints[]>([]);
+  const addWaypoint = (latlng: LatLngExpression) =>
+    setRoute([...route, { name: "test", latlng }]);
 
   return (
     <div className="absolute w-full h-full">
-      <Map position={initPos} zoom={7} onMapClick={handleMapClick} />
+      <Map position={initPos} zoom={7} onMapClick={addWaypoint} />
       <div className="relative h-20 w-96 bottom-40 bg-white z-50 rounded-md left-[calc(50%-24rem)]">
-        {JSON.stringify(coords)}
+        {JSON.stringify(route)}
       </div>
     </div>
   );
