7daa93db15b8b63f9f65541ab588b806d511e46d
Paul Ilaga <pgilaga@gmail.com>
2024-03-23 12:42:49 +0800

    Add signout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { signout } from "@/app/login/actions";
 import { UserProfile } from "@/app/page";
 import { Menu, Transition } from "@headlessui/react";
 import {
@@ -7,11 +8,9 @@ import {
 } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { Fragment } from "react";
+import { Button } from "./common/button";
 
-const userNavigation = [
-  { name: "Your profile", href: "#" },
-  { name: "Sign out", href: "#" },
-];
+const userNavigation = [{ name: "Your profile", href: "#" }];
 
 interface NavigationProps {
   setSidebarOpen: (s: boolean) => void;
@@ -53,55 +52,69 @@ const Navigation: React.FC<NavigationProps> = ({
             aria-hidden="true"
           />
           {/* Profile dropdown */}
-          <Menu as="div" className="relative">
-            <Menu.Button className="-m-1.5 flex items-center p-1.5">
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full bg-gray-50"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-              <span className="hidden lg:flex lg:items-center">
-                <span
-                  className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                  aria-hidden="true"
-                >
-                  {displayName}
-                </span>
-                <ChevronDownIcon
-                  className="ml-2 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
+          {userProfile ? (
+            <Menu as="div" className="relative">
+              <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
                 />
-              </span>
-            </Menu.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
-                        className={clsx(
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900"
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    )}
+                <span className="hidden lg:flex lg:items-center">
+                  <span
+                    className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                    aria-hidden="true"
+                  >
+                    {displayName}
+                  </span>
+                  <ChevronDownIcon
+                    className="ml-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                  {userNavigation.map((item) => (
+                    <Menu.Item key={item.name}>
+                      {({ active }) => (
+                        <a
+                          href={item.href}
+                          className={clsx(
+                            active ? "bg-gray-50" : "",
+                            "block px-3 py-1 text-sm leading-6 text-gray-900"
+                          )}
+                        >
+                          {item.name}
+                        </a>
+                      )}
+                    </Menu.Item>
+                  ))}
+                  <Menu.Item>
+                    <a
+                      className={
+                        "hover:bg-gray-50 block px-3 py-1 text-sm leading-6 text-gray-900"
+                      }
+                      onClick={async () => await signout()}
+                    >
+                      Sign out
+                    </a>
                   </Menu.Item>
-                ))}
-              </Menu.Items>
-            </Transition>
-          </Menu>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          ) : (
+            <a href="/login">Sign in</a>
+          )}
         </div>
       </div>
     </div>
