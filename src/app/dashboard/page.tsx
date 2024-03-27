14c6b94ef2ed3e800a8ef180674530f1f9d771de
Paul Ilaga <pgilaga@gmail.com>
2024-03-27 10:45:08 +0800

    Add toaster handler

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,21 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@/components/Button";
 import { NextPage } from "next";
 import NewRoute from "@/app/dashboard/NewRoute";
+import toast from "react-hot-toast";
 
 const Home: NextPage = () => {
   const [isNewRouteOpen, setIsNewRouteOpen] = useState(false);
   const openNewRoute = () => setIsNewRouteOpen(true);
+
   return (
     <div className="flex w-full h-full items-center justify-center">
       <div>
         <Button onClick={openNewRoute}>Create a route</Button>
+      </div>
+      <div className="ml-4">
+        <Button onClick={() => toast("hello")}>Create a toast</Button>
       </div>
       <NewRoute open={isNewRouteOpen} setOpen={setIsNewRouteOpen} />
     </div>
