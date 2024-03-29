feb14633156d864b4e17e928283051310dc1e9f5
Paul Ilaga <pgilaga@gmail.com>
2024-03-29 08:26:19 +0800

    Remove semicolon

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Loading: NextPage = () => {
   return (
     <div className="flex flex-col grow items-center justify-center">
-      <Spinner />;
+      <Spinner />
     </div>
   );
 };
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Loading: NextPage = () => {
   return (
     <div className="flex flex-col h-full items-center justify-center">
-      <Spinner />;
+      <Spinner />
     </div>
   );
 };
